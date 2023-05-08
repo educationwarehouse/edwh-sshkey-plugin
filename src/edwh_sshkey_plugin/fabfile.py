965de2e7690f90bffa15819180520d574552a1f5
SvenKeimpema <keimpje1234@gmail.com>
2023-05-08 15:44:24 +0200

    fix: add_to_remote and delete_remote fixed so they actually add and remove the keys. they did nothing previously due to me not being able to test the functions from home :/

--- src/edwh_sshkey_plugin/fabfile.py
+++ src/edwh_sshkey_plugin/fabfile.py
@@ -1,29 +1,33 @@
 from fabric import task
 from datetime import datetime
-from tabulate import tabulate
 from yaml.loader import SafeLoader
-from termcolor import colored
 import pathlib
 import yaml
-import pprint
 import time
 import os
 import subprocess
 import platform
-import fabric
 
-YAML_KEYS_PATH = pathlib.Path("~/.ssh/known_keys.yaml")
+YAML_KEYS_PATH = pathlib.Path("~/.ssh/known_keys.yaml").expanduser()
+
 
 def create_new_keyholder():
-    new_key_holder = open(YAML_KEYS_PATH, "x")
-    new_key_holder.close()
+    with open(YAML_KEYS_PATH, "x") as new_key_holder:
+        new_key_holder.close()
+
+
+def create_new_yaml_file_if_not_exists():
+    if not YAML_KEYS_PATH.exists():
+        print("?")
+        create_new_keyholder()
+
 
 def open_new_keyholder(read: bool):
     if YAML_KEYS_PATH.exists():
-        key_holder = open(YAML_KEYS_PATH, "r" if read else "w")
-        return key_holder
+        return open(YAML_KEYS_PATH, "r" if read else "w")
     else:
         create_new_keyholder()
+
 
 def get_keys_from_keyholder():
     key_holder = open_new_keyholder(True)
@@ -34,8 +38,7 @@ def get_keys_from_keyholder():
         print("please run create to generate a new key")
         exit(255)
 
-    all_key_information = key_db.setdefault('keys')
-    return all_key_information
+    return key_db.setdefault('keys')
     
 
 def get_key_count(keys, command_line_key):
@@ -47,16 +50,19 @@ def add_keys_to_remote(c, command_line_keys, all_key_information):
         if command_line_key not in all_key_information:
             break
 
-        for key, value in all_key_information[command_line_key].items():
-            keys = [name for name in ["datetime", "who@hostname", "message"] if name in key]
-            if len(keys) == 3:
-                c.run(f'echo {value} >> ~/.ssh/authorized_keys')
-                c.run('touch ~/.ssh/keys')
-                c.run('sort -u ~/.ssh/authorized_keys > ~/.ssh/keys')
-                time.sleep(1)
-                c.run('cp ~/.ssh/keys ~/.ssh/authorized_keys')
-                c.run('rm ~/.ssh/keys')
-                print(f'Het is gelukt! De \033[1m{command_line_key}\033[0m key is toegevoegd.')
+        all_key_information_remote_items = dict(all_key_information[command_line_key].items())
+
+        keys = [name for name in ["sleutel", "datetime", "who@hostname", "message"] if
+                name in all_key_information_remote_items]
+
+        if len(keys) == 4:
+            c.run(f'echo \"{all_key_information_remote_items["sleutel"]}\" >> ~/.ssh/authorized_keys')
+            c.run('touch ~/.ssh/keys')
+            c.run('sort -u ~/.ssh/authorized_keys > ~/.ssh/keys')
+            time.sleep(1)
+            c.run('cp ~/.ssh/keys ~/.ssh/authorized_keys')
+            c.run('rm ~/.ssh/keys')
+            print(f'Het is gelukt! De \033[1m{command_line_key}\033[0m key is toegevoegd.')
 
 
 def remote_key_doesnt_exist(c, command_line_keys, all_key_information):
@@ -70,13 +76,13 @@ def remote_key_doesnt_exist(c, command_line_keys, all_key_information):
         # maak de key aan die nog NIET in de yaml file stond
         if input(f"Wil je de {which_key} key aanmaken? [Y/n]: ") in ("y", "Y", ""):
             generate_message = str(input('Wat is het bericht dat je mee wilt geven? Deze MOET: '))
-            if generate_message == '':
+            if not generate_message:
                 print('Je moet een message invullen!')
                 exit(1)
             key_split = new_key.split()
             generate(c, generate_message, owner=key_split[0], hostname=key_split[1],
                             doel=new_key.split()[2] if len(new_key.split()) == 3 else '')
-            
+
             add_to_remote(c, command_line_keys)
 
 # met irerable kan je meerdere cli keys in 1 regel meegeven.
@@ -94,7 +100,7 @@ def add_to_remote(c, command_line_key):
     all_key_information = get_keys_from_keyholder()
 
     # controleert of het aantal command_line_key's wel gelijk staan aan de keys die nodig zijn, zo niet gaat die je vragen in de cli of de onjuiste key wil veranderen
-    key_count = get_key_count()
+    key_count = get_key_count(all_key_information, command_line_key)
 
     if key_count == len(command_line_key):
         add_keys_to_remote(c, command_line_key, all_key_information)
@@ -111,22 +117,21 @@ def delete_remote(c, command_line_keys):
     :param c: Connection object
     :param command_line_key: List of keys to be removed
     """
-    yaml_file = open_new_keyholder(read=True)
-    key_db = yaml.safe_load(yaml_file)
-    all_key_information = key_db.get('keys', {})
+    all_key_information = get_keys_from_keyholder()
 
     for command_line_key in command_line_keys:
         if command_line_key not in all_key_information:
             break
 
-        for key, value in all_key_information[command_line_key].items():
-            if key not in ['datetime', 'who@hostname', 'message']:
-                c.run(f'grep -v "{value}" ~/.ssh/authorized_keys > ~/.ssh/keys')
-                c.run('mv ~/.ssh/keys ~/.ssh/authorized_keys')
-                print(f'Success! The {command_line_key} key has been removed.')
+        ssh_key = dict(all_key_information[command_line_key].items())["sleutel"].replace("\n", "")
+        c.run(f'grep -v "{ssh_key}" ~/.ssh/authorized_keys > ~/.ssh/keys')
+        c.run('mv ~/.ssh/keys ~/.ssh/authorized_keys')
+        print(f'Success! The {command_line_key} key has been removed.')
 
 @task
 def generate(c, message, owner='', hostname='', doel=''):
+    create_new_yaml_file_if_not_exists()
+
     key_name = '-'.join(filter(bool, [owner, hostname, doel]))
     if sum([owner != '', hostname != '', doel != '']) < 2:
         print('Please provide at least two of the following arguments: Owner, Hostname, Doel')
@@ -136,47 +141,52 @@ def generate(c, message, owner='', hostname='', doel=''):
         print(f'{key_name} already exists. Key generation aborted.')
         return
     
-    subprocess.run(f'ssh-keygen -t rsa -b 4096 -f ~/.managed_ssh_keys-{key_name} -N "" -C "{message}"', shell=True)
+    subprocess.run(f'ssh-keygen -t rsa -b 4096 -f ~/.ssh/.managed_ssh_keys-{key_name} -N "" -C "{message}"', shell=True)
     
-    with open('key_holder.yaml', 'a') as f:
+    with open(YAML_KEYS_PATH, 'a') as f:
         yaml.dump({
             'keys': {
                 key_name: {
-                    'sleutel': open(f'~/.managed_ssh_keys-{key_name}.pub').read(),
+                    'sleutel': open(pathlib.Path(f'~/.ssh/.managed_ssh_keys-{key_name}.pub').expanduser()).read(),
                     'datetime': datetime.today().strftime("Datum: %Y-%m-%d Tijdstip: %H:%M:%S"),
                     'who@hostname': f"{os.getlogin()}@{platform.node()}",
                     'message': message
                 }
             }
         }, f, indent=4)
-        
+    # .managed_ssh_keys-sven.edwh.nl-ubuntu.pub
     print(f'Public key saved in ~/.managed_ssh_keys-{key_name}.pub')
 
+
+@task
 def list(c):
     # Load the keys from the YAML file
-    with open('key_holder.yaml', 'r') as yaml_file:
-        key_db = yaml.safe_load(yaml_file)
-        all_key_information = key_db.setdefault('keys')
+    all_key_information = get_keys_from_keyholder()
     
     # Get the list of authorized keys from the remote machine
-    remote_keys = c.run('cat ~/.ssh/authorized_keys', hide=True).stdout
+    if len(c.run("ls ~/.ssh/authorized_keys", warn=True, hide=True).stdout) > 0:
+        remote_keys = c.run('cat ~/.ssh/authorized_keys', hide=True).stdout
+    else:
+        print("[WARNING] remote authorized_keys not found!")
+        remote_keys = ""
     
     # Iterate through the keys and separate them into two lists
     local_keys = []
     remote_known_keys = []
     for key_data in all_key_information.values():
-        for key, value in key_data.items():
-            if 'datetime' in key or 'who@hostname' in key or 'message' in key:
-                if value in remote_keys:
-                    remote_known_keys.append(key_data['head_keys'])
-                else:
-                    local_keys.append(key_data['head_keys'])
+        if key_data['sleutel'] in remote_keys:
+            remote_known_keys.append(key_data['sleutel'])
+        else:
+            local_keys.append(key_data['sleutel'])
     
     # Check for any unrecognized keys in the authorized_keys file
-    unrecognized_keys = c.run(f"grep -v '{'|'.join(remote_known_keys)}' ~/.ssh/authorized_keys", hide=True)
-    if unrecognized_keys.stdout.strip():
+    if pathlib.Path("~/.ssh/authorized_keys").expanduser().exists():
+        unrecognized_keys = c.run(f"grep -v '{'|'.join(remote_known_keys)}' ~/.ssh/authorized_keys", hide=True).stdout
+    else:
+        unrecognized_keys = str(remote_known_keys).replace("[", "").replace("]", "")
+    if len(unrecognized_keys.strip()) > 0:
         print('Unrecognized keys found in authorized_keys:')
-        print(unrecognized_keys.stdout)
+        print(unrecognized_keys)
     
     # Display the keys in a table
     if local_keys:
